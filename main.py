daa59ee231c8efe807357d24960a10e7e1512087
lunacao <41524369+JosukeCrazyDiamond@users.noreply.github.com>
2018-10-02 01:27:42 -0400

    register function

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, url_for
-
+from flask import Flask, render_template, url_for,flash,redirect
+from forms import RegForm, LogForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '11f915b5b49df409af92b1b8817e5a60'
 
 posts = [
     {
@@ -19,7 +21,8 @@ posts = [
 
 
 @app.route('/')
-def hello():
+@app.route('/home')
+def home():
     return render_template('home.html', posts=posts)
 
 
@@ -27,6 +30,18 @@ def hello():
 def about():
     return render_template('about.html', title='about')
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='register', form=form)
+
+@app.route('/login')
+def login():
+    form = LogForm()
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
     app.run('0.0.0.0', debug=True)
